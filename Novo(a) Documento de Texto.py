b7f55811ce656b1dcea81e5cdc3dfa42fd2160af
Deleon <delps.santos1987@gmail.com>
2025-01-08 21:39:53 -0300

    continuação de temas

--- Novo(a) Documento de Texto.py
+++ Novo(a) Documento de Texto.py
@@ -15,6 +15,7 @@ class Janela(ctk.CTk):
         super().__init__()#todas as funcionalidades devem ser chamadas apartir da superclass
         self.layout()
         self.frame_master()
+        self.aparencia()
         
     #configuração do layout
     def layout(self):
@@ -24,7 +25,9 @@ class Janela(ctk.CTk):
 
     #configuração dos modos de aparencia
     def aparencia(self):
-        pass
+        self.label_modo = ctk.CTkLabel(self, text=('Tema'), bg_color='transparent',text_color=['#000','#fff'],font=('ariel',15)).place(x=90, y=500)
+        self.opt_modo = ctk.CTkOptionMenu(self, values=['Light','Dark','Sistem'], command= self.cambiar).place(x=90, y=540)
+    
 
     #frame onde vai rodar o sistema vais receber todos os entrys e labels e botões 
     def frame_master(self):
@@ -34,7 +37,12 @@ class Janela(ctk.CTk):
         entry_nome = ctk.CTkEntry(self, font=('ariel',20),width=450).place(x=90,y=180)
         label_tel = ctk.CTkLabel(self,text='Tel/Celular', text_color='#fff',font=('ariel',15)).place(x=590, y=150)
         entry_tel = ctk.CTkEntry(self, font=('ariel',20),width=150).place(x=590,y=180)
-        
+        label_mail = ctk.CTkLabel(self,text='E-mail', font=('ariel',15),text_color='#fff').place(x=90,y=290)
+        label_idade = ctk.CTkLabel(self,text='Idade', text_color='#fff',font=('ariel',15)).place(x=550, y=290)
+        label_genero = ctk.CTkLabel(self,text='Genero', text_color='#fff',font=('ariel',15)).place(x=620, y=290)
+        entry_mail = ctk.CTkEntry(self, font=('ariel',20),width=430).place(x=90,y=320)
+        entry_idade = ctk.CTkEntry(self, font=('ariel',20),width=40).place(x=550,y=320)
+        label_nome = ctk.CTkComboBox(self,values=['Masculino','Feminino'], width=120, text_color='#fff',font=('ariel',15)).place(x=620, y=320)
         
         #função para pegar os valores
         def pegar_valores(self):
@@ -45,8 +53,10 @@ class Janela(ctk.CTk):
             pass
 
     #função para alterna a aparencia do sistema
-    def cambiar_modo(self):
-        pass
+    def cambiar(self,nova_aparencia):
+        ctk.set_appearance_mode(nova_aparencia)
+    
+
 
 
 
